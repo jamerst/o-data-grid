8c4c9cabd7d38e9c1c97baacdfafa3931c982004
James Tattersall <10601770+jamerst@users.noreply.github.com>
2022-01-03 15:42:20 +0000

    Fix package exports

--- packages/o-data-grid-pro/src/index.ts
+++ packages/o-data-grid-pro/src/index.ts
@@ -3,16 +3,18 @@ import { GridColDef } from "@mui/x-data-grid-pro";
 import ODataGridPro from "ODataGridPro";
 import { ODataGridBaseColDef } from "../../base/types";
 import FilterBuilder from "../../base/FilterBuilder/components/FilterBuilder";
+import { allOperators, numericOperators } from "../../base/FilterBuilder/constants";
 
 export {
   ODataGridPro,
-  FilterBuilder
+  FilterBuilder,
+  allOperators,
+  numericOperators
 }
 
+export type { ODataGridProProps } from "ODataGridProProps"
 export type ODataGridColDef = ODataGridBaseColDef<GridColDef>
 
 export type { SelectOption, ValueOption } from "../../base/types";
 export type { CollectionFieldDef, CollectionOperation, Connective, FilterBuilderLocaleText, FieldDef, Group, Condition, QueryStringCollection } from "../../base/FilterBuilder/types";
 export type { FilterBuilderProps } from "../../base/FilterBuilder/components/FilterBuilder";
-
-export type { allOperators, numericOperators } from "../../base/FilterBuilder/constants";
--- packages/o-data-grid/src/index.ts
+++ packages/o-data-grid/src/index.ts
@@ -3,16 +3,18 @@ import { GridColDef } from "@mui/x-data-grid";
 import ODataGrid from "ODataGrid";
 import { ODataGridBaseColDef } from "../../base/types";
 import FilterBuilder from "../../base/FilterBuilder/components/FilterBuilder";
+import { allOperators, numericOperators } from "../../base/FilterBuilder/constants";
 
 export {
   ODataGrid,
-  FilterBuilder
+  FilterBuilder,
+  allOperators,
+  numericOperators
 }
 
+export type { ODataGridProps } from "ODataGridProps";
 export type ODataGridColDef = ODataGridBaseColDef<GridColDef>
 
 export type { SelectOption, ValueOption } from "../../base/types";
 export type { CollectionFieldDef, CollectionOperation, Connective, FilterBuilderLocaleText, FieldDef, Group, Condition, QueryStringCollection } from "../../base/FilterBuilder/types";
 export type { FilterBuilderProps } from "../../base/FilterBuilder/components/FilterBuilder";
-
-export type { allOperators, numericOperators } from "../../base/FilterBuilder/constants";
